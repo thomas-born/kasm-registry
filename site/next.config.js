68ec885e65374a9b105c36e6486bb7725eb7bc89
thomas-born <20229739+thomas-born@users.noreply.github.com>
2026-07-20 14:55:59 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Bornemanns Repository',
+    description: 'Bornemanns Place for Kasm workspaces.',
     icon: 'https://user-images.githubusercontent.com/5698566/230345149-ef757e51-6eb9-479d-94f5-a13e4ad33b03.png',
     listUrl: 'https://thomas-born.github.io/kasm-registry',
     contactUrl: 'https://thomas-born.github.io/kasm-registry/issues',
